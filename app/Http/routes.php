e947681fae275f3af1642802ecbf6fcb012d4a13
me_deb <dbjtsrkr672@gmail.com>
2016-05-28 03:54:04 +0530

    Error handler uploaded

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,9 +4,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('tags','TagController@search');
-Route::POST('findTag','TagController@find');
-Route::GET('adtag','TagController@addTag');
-Route::POST('addTag','TagController@add');
-
+/*
+Do your code her
+*/
